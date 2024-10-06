b0d1d91bfd44a0275719d32e31793e1fad9bd173
luqman-bashir <uthamin98@gmail.com>
2024-10-06 10:13:11 +0300

    moving dodgers

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+
+function moveDodgerLeft() {  
+    
+    const currentLeftPosition = dodger.style.left;  
+    
+     
+    const left = parseInt(currentLeftPosition, 10);  
+  
+    if (left > 40) {  
+     
+      dodger.style.left = (left - 1) + 'px';  
+    }  
+  }
+
+
+  function moveDodgerRight() {  
+  
+    const leftNumbers = dodger.style.left.replace("px", "");  
+    const left = parseInt(leftNumbers, 10); 
+   
+    if (left < 360) { 
+      dodger.style.left = `${left + 1}px`;  
+    }  
+  }
+
+
